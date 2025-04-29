ff1d7b7f73c8f6009f14b2262d6f63b5ed24f7f3
bobby reed <37113385+bobbyreed@users.noreply.github.com>
2025-04-29 15:25:12 -0500

    completed basic calculator functionality.
    I'll probably do a couple of more commits. I want to make it look good and set it up to be loaded into other pages well. I should probably integrate react to use useState for the bools and make use of the Component structure to pull this into other websites.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# calculator
+# Calculator
 Calculator built as part of the Odin Project Curriculum
+
+## Features
+Typical calculator features. May expand in the future.
+
+## Use
+See live demo here.
---
+++ app.js
@@ -0,0 +1,119 @@
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearDisplay);
+var inputReady = true;
+var joinReady = false;
+var operatorAdded = false;
+
+//switch declarations to a loop
+const button0 = document.getElementById("0");
+button0.addEventListener("click", function() { numPress(0); });
+const button1 = document.getElementById("1");
+button1.addEventListener("click", function() { numPress(1); });
+const button2 = document.getElementById("2");
+button2.addEventListener("click", function() { numPress(2); });
+const button3 = document.getElementById("3");
+button3.addEventListener("click", function() { numPress(3); });
+const button4 = document.getElementById("4");
+button4.addEventListener("click", function() { numPress(4); });
+const button5 = document.getElementById("5");
+button5.addEventListener("click", function() { numPress(5); });
+const button6 = document.getElementById("6");
+button6.addEventListener("click", function() { numPress(6); });
+const button7 = document.getElementById("7");
+button7.addEventListener("click", function() { numPress(7); });
+const button8 = document.getElementById("8");
+button8.addEventListener("click", function() { numPress(8); });
+const button9 = document.getElementById("9");
+button9.addEventListener("click", function() { numPress(9); });
+const buttonPlus = document.getElementById("+");
+buttonPlus.addEventListener("click", function() { operatorPress("+"); });
+const buttonMinus = document.getElementById("-");
+buttonMinus.addEventListener("click", function() { operatorPress("-"); });
+const buttonMultiply = document.getElementById("*");
+buttonMultiply.addEventListener("click", function() { operatorPress("*"); });
+const buttonDivide = document.getElementById("/");
+buttonDivide.addEventListener("click", function() { operatorPress("/"); });
+const buttonEquals = document.getElementById("=");
+buttonEquals.addEventListener("click", solve);
+
+function clearDisplay() {
+    updateDisplay("");
+    inputJoinSwitch();
+}
+
+function inputJoinSwitch(){
+    if(inputReady) {
+        inputReady = false;
+        joinReady = true;
+    } else if(joinReady) {
+        inputReady = true;
+        joinReady = false;
+    }
+}
+
+function numPress(num){
+    let currentNum = document.getElementById("lcd").innerHTML;
+    //console.log("currentNum is " + currentNum);
+    if(inputReady){
+        updateDisplay(num);
+        inputJoinSwitch();
+    }
+    if(joinReady){
+        console.log("num is " + num + ". currentNum is " + currentNum);
+        num = currentNum.toString() + num.toString();
+        console.log("Combined, num is " + num);
+        updateDisplay(num);
+    }  
+}
+
+function operatorPress(operator) {
+    let currentNum = document.getElementById("lcd").innerHTML;
+    operatorAdded = true;
+    //console.log("currentNum is " + currentNum);
+    if(inputReady){
+        return;
+    }
+    if(joinReady){
+        console.log("operator is " + operator + ". currentNum is " + currentNum);
+        operator = currentNum.toString() + " " + operator.toString() + " ";
+        updateDisplay(operator);
+    }  
+}
+
+function solve() {
+    let currentNum = document.getElementById("lcd").innerHTML;
+    currentNum = currentNum.toString().trim();
+    let numArray = currentNum.split(" ");
+    for(let i = 0; i < numArray.length; i++) {
+        console.log("numArray[" + i + "] is " + numArray[i]);
+    }
+    switch(numArray[1]){
+        case "+":
+            var solution = parseFloat(numArray[0]) + parseFloat(numArray[2]);
+            break;
+        case "-":
+            var solution = parseFloat(numArray[0]) - parseFloat(numArray[2]);
+            break;
+        case "*":
+            var solution = parseFloat(numArray[0]) * parseFloat(numArray[2]);
+            break;
+        case "/":
+            if (parseFloat(numArray[2]) === 0) {
+                alert("Cannot divide by zero.");
+                return;
+            }
+            var solution = parseFloat(numArray[0]) / parseFloat(numArray[2]);
+            break;
+        default:
+            alert("Invalid operator.");
+            return;
+    }
+    updateDisplay(solution);
+}
+
+function updateDisplay(num) {
+   let element = document.getElementById("lcd");
+   element.innerHTML = num;
+}
+
+export default { clearDisplay, updateDisplay };
